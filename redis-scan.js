7a0b0451e874c5668efe592a5b50205d09f0dbde
Locke <locke@gigsalad.com>
2019-10-21 17:30:49 -0400

    Add support for additional scan commands
    Each version of HSCAN, SSCAN, and ZSCAN are now available.
    
    Also add support for setting the scan command COUNT option.
    
    Resolves #1

--- redis-scan.js
+++ redis-scan.js
@@ -27,38 +27,62 @@ class RedisScan {
 	 * @method
 	 *
 	 * @param {String} pattern - A Redis glob-style string pattern of keys to
-	 * match.
+	 * match. See: https://redis.io/commands/scan#the-match-option
 	 *
 	 * @param {Function} eachScanCallback - A function called after each
 	 * call to the Redis `SCAN` command. Invoked with (matchingKeys).
 	 *
 	 * @param {Function} [callback] - A function called after the full scan has
 	 * completed and all keys have been returned.
+	 *
+	 * @param {Object} options - A set of options for controlling the scan.
+	 * The following options are available:
+	 * - method {String} The string name ofthe method for this scan.
+	 * Used for performing scans of a hash (HSCAN), set (SSCAN), or sorted set (ZSCAN).
+	 * - key {String} The key name used for HSCAN, SSCAN, or ZSCAN.
+	 * - count {Number} The amount of "work" we want to do with each
+	 * iteration of the given SCAN command. Often speeds up scans to increase
+	 * this to hundreds or thousands when working with huge keyspaces.
+	 * See: https://redis.io/commands/scan#the-count-option
 	 */
-	eachScan(pattern, eachScanCallback, callback) {
-		let matchingKeysCount = 0;
+	eachScan(pattern, eachScanCallback, callback, options = {}) {
+		const method = options.method || 'scan';
+		const key = options.key || '';
+		const count = options.count || 0;
+
+		let matchesCount = 0;
 
 		// Because we're using the `scan()` method of the node-redis library
 		// a recursive function seems easiest here.
 		const recursiveScan = (cursor = 0) => {
-			// Build a Redis `SCAN` command using the `MATCH` option.
-			// See: https://redis.io/commands/scan#the-match-option
-			this.redisClient.scan(cursor, 'MATCH', pattern, (err, data) => {
+			// Build `SCAN` command parameters using the `MATCH` option
+			// and a possible key.
+			let parameters = [cursor, 'MATCH', pattern];
+			if (key) {
+				parameters = [key, cursor, 'MATCH', pattern];
+			}
+
+			// Add any custom `COUNT` scan option.
+			if (count > 0) {
+				parameters.push('COUNT', count);
+			}
+
+			this.redisClient[method](...parameters, (err, data) => {
 				if (err) {
 					callback(err);
 				} else {
-					// client.scan() returns an array with two elements. The
+					// Scan calls return an array with two elements. The
 					// first element is the next cursor and the second is an
-					// array of matching keys (which might be empty). We'll
+					// array of matches (which might be empty). We'll
 					// destructure this into two variables.
-					const [cursor, matchingKeys] = data;
+					const [cursor, matches] = data;
 
-					matchingKeysCount += matchingKeys.length;
-					eachScanCallback(matchingKeys);
+					matchesCount += matches.length;
+					eachScanCallback(matches);
 
 					// We're done once Redis returns 0 for the next cursor.
 					if (cursor === '0') {
-						callback(null, matchingKeysCount);
+						callback(null, matchesCount);
 					} else {
 						// Otherwise, call this function again AKA recurse
 						// and pass the next cursor.
@@ -89,21 +113,60 @@ class RedisScan {
 	 * @param {Function} [callback] - A function called after the full scan
 	 * of the Redis keyspace completes having searched for the given pattern.
 	 * Invoked with (err, keys).
+	 *
+	 * @param {Object} options - See eachScan options parameter documentation.
 	 */
-	scan(pattern, callback) {
+	scan(pattern, callback, options = {}) {
 		let keys = [];
 
 		// Collect all our keys into a single array using the `eachScan()`
 		// method from above.
 		this.eachScan(pattern, (matchingKeys) => {
 			keys = keys.concat(matchingKeys);
-		}, (err, count) => {
+		}, (err) => {
 			if (err) {
 				callback(err);
 			} else {
 				callback(null, keys);
 			}
-		});
+		}, options);
+	}
+
+	/**
+	 * Scans a Redis hash keyspace to find matching hash keys. The matching
+	 * keys and values are returned, in sequence, as an array of strings via
+	 * callback.
+	 *
+	 * @name hscan
+	 *
+	 * @method
+	 *
+	 * @param {String} key
+	 * @param {String} pattern
+	 * @param {Function} [callback]
+	 */
+	hscan(key, pattern, callback) {
+		this.scan(pattern, callback, {method: 'hscan', key: key});
+	}
+
+	eachHScan(key, pattern, eachScanCallback, callback) {
+		this.eachScan(pattern, eachScanCallback, callback, {method: 'hscan', key: key});
+	}
+
+	sscan(key, pattern, callback) {
+		this.scan(pattern, callback, {method: 'sscan', key: key});
+	}
+
+	eachSScan(key, pattern, eachScanCallback, callback) {
+		this.eachScan(pattern, eachScanCallback, callback, {method: 'sscan', key: key});
+	}
+
+	zscan(key, pattern, callback) {
+		this.scan(pattern, callback, {method: 'zscan', key: key});
+	}
+
+	eachZScan(key, pattern, eachScanCallback, callback) {
+		this.eachScan(pattern, eachScanCallback, callback, {method: 'zscan', key: key});
 	}
 }
 
